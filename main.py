c4053f20b2b1c478c454828852191cbf6d05c17a
rkralik <rkralik96@gmail.com>
2024-12-29 11:00:30 +0100

    transformed character count dictionary to list of dictionaries, sorted, finished the report functions in main

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ def main():
     text=get_text(book_path)
     word_count=count(text)
     character_count=char_count(text)
-    character_count.sort(reverse=True, key=sorting_key_number)
+    character_count_list=dictionary_to_listofdictionaries(character_count)
+    character_count_list.sort(reverse=True, key=sorting_key_number)
     print(f"--Begin report of {book_path}--")
     print(f"{word_count} words found in the document")
-    print(character_count)
+    alpha_report(character_count_list)
 
 #the read function, returns the book as a string
 def get_text(path):
@@ -31,10 +32,29 @@ def char_count(text):
         else: lowercase_dictionary[char] = 1
     return lowercase_dictionary
 
+#turn dictionary into a list of dictionaries
+
+def dictionary_to_listofdictionaries(dict):
+    turned_list=[]
+    sub_dict={}
+    for char in dict:
+        sub_dict["character"]=char
+        sub_dict["count"]=dict[char]
+        turned_list.append(sub_dict)
+        sub_dict={}
+    return turned_list
+
+
 #the sorting key function, returns a number of appearances of each character, used in dictionary ordering
 def sorting_key_number(dict):
-    for char in dict:
-        return dict[char]
+    return dict["count"]
+
+#alphabetical characters generating report
+def alpha_report(list):
+    for dict in list:
+        if dict["character"].isalpha()==True:
+            print(f"The '{dict["character"]}' character was found {dict["count"]} times")
+
 
 
 main()
